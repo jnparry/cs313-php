49029014afad4949d4b59b5e6a42d438b0d2cca8
Jordan <par16042@byui.edu>
2018-05-01 16:36:32 -0600

    Class test php file

---
+++ web/test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>My PHP Title Page</title>
+        
+        <script>
+        </script>
+        
+        <style>
+        </style>
+    </head>
+    
+    <body>
+        <p>This is a php page</p>
+        <?php
+            $x = 4 + "cat";
+            echo "<p>$x</p>";
+        ?>
+    </body>
+</html>
