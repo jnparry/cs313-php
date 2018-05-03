b4a25971fc8aa9f8330990d8a0a3e8f964e02d55
Jordan <par16042@byui.edu>
2018-05-03 16:27:20 -0600

    css linked progress

--- web/ta02/navbar.php
+++ web/ta02/navbar.php
@@ -1,11 +1,13 @@
 <link rel="stylesheet" href="index.css">
 
+<?php 
+    $uri = $_SERVER['REQUEST_URI']; 
+?>
+
 <h1>PurchasePlanets.com</h1>
 
 <nav>
-    <a href="home.php" >Home</a>
+    <a href="home.php" <?php if ($uri == "/ta02/home.php") { echo "class='active'";} ?>>Home</a>
     <a href="about-us.php">About Us</a>
     <a href="login.php">Login</a>
 </nav>
-
-<?php echo ($_SERVER['REQUEST_URI']); ?>
