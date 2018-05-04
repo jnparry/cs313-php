49ed784e55fd32a6dc3825ea6f9b08da92f0fefb
Jordan <par16042@byui.edu>
2018-05-04 13:16:19 -0600

    Homepage progress

--- web/homepage/nav.php
+++ web/homepage/nav.php
@@ -1,6 +1,6 @@
 <nav>
-    <a href="home.php" <?php if ($uri == "/ta02/home.php") { echo "class='active'";} ?>>Home</a>
-    <a href="about.php" <?php if ($uri == "/ta02/about.php") { echo "class='active'";} ?>>About</a>
-    <a href="assign.php" <?php if ($uri == "/ta02/assign.php") { echo "class='active'";} ?>>Assignments</a>
-    <a class="active" <?php if ($uri == "/ta02/contact.php") { echo "class='active'";} ?>>Contact</a>
+    <a href="home.php" <?php if ($uri == "/homepage/home.php") { echo "class='active'";} ?>>Home</a>
+    <a href="about.php" <?php if ($uri == "/homepage/about.php") { echo "class='active'";} ?>>About</a>
+    <a href="assign.php" <?php if ($uri == "/homepage/assign.php") { echo "class='active'";} ?>>Assignments</a>
+    <a class="active" <?php if ($uri == "/homepage/contact.php") { echo "class='active'";} ?>>Contact</a>
 </nav>
