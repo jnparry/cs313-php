a46038dbb9ec4494ebd4a78617732e7c5b28d926
Jordan <par16042@byui.edu>
2018-05-04 13:30:40 -0600

    Homepage progress

--- web/homepage/about.php
+++ web/homepage/about.php
@@ -10,10 +10,7 @@
         <?php require "nav.php" ?>
         
         <div>
-            <h2 style="text-align: center;">About Me</h2>
-            <p><span>Name</span>: Jordan Parry</p>
-            <p><span>Major</span>: Computer Science</p>
-            <p><span style="font-weight: bold; text-decoration: underline;">Home Town</span>: Idaho Falls, Idaho</p>
+            <h2>About Me</h2>
         </div>
     </body>
 </html>
