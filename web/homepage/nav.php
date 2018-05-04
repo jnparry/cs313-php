9bf80a5b9de7798f59dabca50ea77921d976a541
Jordan <par16042@byui.edu>
2018-05-04 13:27:26 -0600

    Homepage progress

--- web/homepage/nav.php
+++ web/homepage/nav.php
@@ -1,4 +1,5 @@
 <nav>
+    <?php $uri = $_SERVER['REQUEST_URI']; ?>
     <a href="home.php" <?php if ($uri == "/homepage/home.php") { echo "class='active'";} ?>>Home</a>
     <a href="about.php" <?php if ($uri == "/homepage/about.php") { echo "class='active'";} ?>>About</a>
     <a href="assign.php" <?php if ($uri == "/homepage/assign.php") { echo "class='active'";} ?>>Assignments</a>
