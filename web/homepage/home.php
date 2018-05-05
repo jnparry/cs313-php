e0c4389a9b04b1978832afd47aca220986189d17
Jordan <par16042@byui.edu>
2018-05-05 09:33:45 -0600

    Homepage progress

--- web/homepage/home.php
+++ web/homepage/home.php
@@ -16,7 +16,7 @@
             <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22196364_1531234120303010_5342914097511881599_n.jpg?_nc_cat=0&oh=6294f300d8cbce483acf9510b3fc4cf9&oe=5B5649E6" alt="Jordan Parry Homepage Image - Sunflowers">
         </div>
         
-        <blockquote>"In a field
+        <blockquote>"In a field 
             I am the absence
             of field.
             This is
