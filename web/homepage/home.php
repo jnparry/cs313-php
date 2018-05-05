f4b10ae4493a48575f56d233d3ee14ee80d017f6
Jordan <par16042@byui.edu>
2018-05-05 09:32:25 -0600

    Homepage progress

--- web/homepage/home.php
+++ web/homepage/home.php
@@ -12,6 +12,29 @@
     <body>
         <?php require "nav.php" ?>
         
-        <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22196364_1531234120303010_5342914097511881599_n.jpg?_nc_cat=0&oh=6294f300d8cbce483acf9510b3fc4cf9&oe=5B5649E6" alt="Jordan Parry Homepage Image - Sunflowers">
+        <div>
+            <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22196364_1531234120303010_5342914097511881599_n.jpg?_nc_cat=0&oh=6294f300d8cbce483acf9510b3fc4cf9&oe=5B5649E6" alt="Jordan Parry Homepage Image - Sunflowers">
+        </div>
+        
+        <blockquote>"In a field
+            I am the absence
+            of field.
+            This is
+            always the case.
+            Wherever I am
+            I am what is missing.
+
+            When I walk
+            I part the air
+            and always
+            the air moves in   
+            to fill the spaces
+            where my body’s been.
+
+            We all have reasons
+            for moving.
+            I move
+            to keep things whole." - Mark Strand
+        </blockquote>
     </body>
 </html>
