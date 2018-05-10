ad6496a790c1370a7ab01e23dff857d44230cc1d
Jordan <par16042@byui.edu>
2018-05-10 16:40:25 -0600

    Session

---
+++ web/class/session.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>My Title Page</title>
+        <link rel="stylesheet" href="">
+        <script src=""></script>
+    </head>
+    
+    <body>
+<!--
+        <?php
+            if ((isset($_SESSION["count"])) {
+                $_SESSION["count"] = 0;
+            }
+            $_SESSION["count"] = $_SESSION["count"] + 1;
+        ?>
+-->
+        <p>You have visited this page times</p>
+    </body>
+</html>
+    
+    
