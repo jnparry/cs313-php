71ce3559ab60815fa2418dc2af305a3f4cca838f
Jordan <par16042@byui.edu>
2018-05-10 16:36:15 -0600

    Session

--- web/session.php
+++ web/session.php
@@ -13,10 +13,10 @@
     
     <body>
         <?php
-            if (!(isset($_SESSION["count"])) {
+            if ((isset($_SESSION["count"])) {
                 $_SESSION["count"] = 0;
             }
-            $_SESSION["count"]++;
+            $_SESSION["count"] = $_SESSION["count"] + 1;
         ?>
         <p>You have visited this page times</p>
     </body>
