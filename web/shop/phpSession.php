cfa9621401a126c4d68c529824b223070eb08664
Jordan <par16042@byui.edu>
2018-05-10 20:03:47 -0600

    Shopping

--- web/shop/phpSession.php
+++ web/shop/phpSession.php
@@ -5,3 +5,17 @@ if(!isset($_SESSION['cart'])){
 $_SESSION['cart'][] = $_POST['passedVal'];
 
 var_dump($_SESSION);
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Shop | Home</title>
+        <link rel="stylesheet" href="index.css">
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    </head>
+    
+    <body>
+        <p>so what happened?</p>
+    </body>
+</html>
