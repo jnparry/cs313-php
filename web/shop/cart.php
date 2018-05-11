0a70508c49b93a7a77e2278c136c99539a5aaa4f
Jordan <par16042@byui.edu>
2018-05-10 23:26:49 -0600

    View cart

--- web/shop/cart.php
+++ web/shop/cart.php
@@ -1,14 +1,28 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Shop | Cart</title>
         <link rel="stylesheet" href="index.css">
-        <script src=""></script>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
     </head>
     
     <body>
         <?php require "navbar.php"?>
         <h1>Cart</h1>
+        <p>You ordered: 
+            <?php
+                echo count($_SESSION['cart']);
+                foreach ($_SESSION['cart'] as $value) {
+                    echo "<p>";
+                    echo $value;
+                    echo "</p>";
+                }
+            ?>
+        </p>
     </body>
 </html>
--- web/shop/phpSession.php
+++ web/shop/phpSession.php
@@ -25,26 +25,3 @@
     header("Location: https://stormy-cove-35722.herokuapp.com/shop/home.php"); /* Redirect browser */
     exit();
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Shop | Home</title>
-        <link rel="stylesheet" href="index.css">
-        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-    </head>
-    
-    <body>
-        <p>You ordered: 
-            <?php
-                echo count($_SESSION['cart']);
-                foreach ($_SESSION['cart'] as $value) {
-                    echo "<p>";
-                    echo $value;
-                    echo "</p>";
-                }
-            ?>
-        </p>
-    </body>
-</html>
