2f648a48f0fae4ad3c353716de24e32d39794c10
Jordan <par16042@byui.edu>
2018-05-11 08:48:33 -0600

    remove button

--- web/shop/phpRemove.php
+++ web/shop/phpRemove.php
@@ -4,16 +4,21 @@
     if(isset($_SESSION['cart'])){
 
         $item = $_POST['yourItem'];
-        unset($_SESSION['cart'][$item]);
-    }
 
-    else {
-        echo "not set?";
-    }
+//foreach($_SESSION as $key => $value) {
+//    unset($_SESSION[$key]);
+//}
 
-    foreach ($_SESSION['cart'] as $value) {
-        echo "Items are: ";
-        echo $value;
+        foreach ($_SESSION['cart'] as $value) {
+            if ($value === $item) {
+                unset($_SESSION['cart'][$value]);
+            }
+        }
+
+        foreach ($_SESSION['cart'] as $value) {
+            echo "Items are: ";
+            echo $value;
+        }
     }
 
 //    header("Location: https://stormy-cove-35722.herokuapp.com/shop/cart.php"); /* Redirect browser */
