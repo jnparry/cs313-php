8cc2f33578e7494836af821f84d52d31881cf781
Jordan <par16042@byui.edu>
2018-05-11 10:35:55 -0600

    checkout

--- web/shop/confirm.php
+++ web/shop/confirm.php
@@ -18,7 +18,6 @@
         <meta charset="utf-8">
         <title>Shop | Confirmation</title>
         <link rel="stylesheet" href="index.css">
-        <script src="index.css"></script>
     </head>
     
     <body>
