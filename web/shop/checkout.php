ec99923418cf28e3f44c1426bc79f617d81826f7
Jordan <par16042@byui.edu>
2018-05-11 10:21:53 -0600

    checkout

--- web/shop/checkout.php
+++ web/shop/checkout.php
@@ -17,15 +17,15 @@
                 <p id="invalid"></p>
                 <h2>Customer Information</h2>
                 <br>
-                <input type="text" name="form-info" id="fname" onload="setFocus()" placeholder="First Name" pattern="[a-zA-Z]*" required>
+                <input type="text" name="form-info" id="fname" onload="setFocus()" placeholder="First Name" pattern="[a-zA-Z]*" title="First name - letters only." required>
                 <br><br>
-                <input type="text" name="form-info" id="lname" placeholder="Last Name" pattern="[a-zA-Z]*" required>
+                <input type="text" name="form-info" id="lname" placeholder="Last Name" pattern="[a-zA-Z]*" title="Last name - letters only." required>
                 <br><br>
-                <input type="text" id="street" name="address" placeholder="Street" required>
+                <input type="text" id="street" name="address" placeholder="Street" pattern="\d+\w*\s*(?:[\-\/]?\s*)?\d*\s*\d+\/?\s*\d*\s*" title="Street address." required>
                 <br>
-                <input type="text" id="city" name="address" placeholder="City, State" pattern="[a-zA-Z]+,[ ][A-Z]{2}" required>
+                <input type="text" id="city" name="address" placeholder="City, State" title="City, State - ex: Rexburg, ID" pattern="[a-zA-Z]+,[ ][A-Z]{2}" required>
                 <br>
-                <input type="text" id="zip" rows="1" cols="50" name="address" placeholder="Zipcode" pattern="[0-9]{5}" required>
+                <input type="text" id="zip" name="address" placeholder="Zipcode" pattern="[0-9]{5}" title="Zipcode - 5 numbers only." required>
                 <br><br>
 
                 <button type="reset">Reset</button>
