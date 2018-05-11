cce6ebfe9e814209b1138d8aab8f296ff0e38945
Jordan <par16042@byui.edu>
2018-05-10 22:53:43 -0600

    Shopping

--- web/shop/home.js
+++ web/shop/home.js
@@ -49,7 +49,7 @@ window.onclick = function(event) {
 
 function setItem(name) {
     document.getElementById("selectedItem").value = name;
-    alert(document.getElementById("selectedItem".value));
+    alert(name);
 }
 
 function add(item) {
