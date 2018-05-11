0b9fe0c49af0681d09cf4c5ae5c6354080d23dde
Jordan <par16042@byui.edu>
2018-05-11 07:03:59 -0600

    pull up pictures in cart

--- web/shop/cart.php
+++ web/shop/cart.php
@@ -37,30 +37,29 @@
                             }
                             else if ($value === "Deity Mask") {
                                 echo "<p>Deity Mask</p>
-                                    <img id=\"pic\" src=\"https://pre00.deviantart.net/fcbf/th/pre/f/2012/032/2/6/fierce_deity__s_mask_by_blueamnesiac-d4ob1ax.png\" alt=\"Zelda Diety Mask\">";
+                                    <img id=\"pic\" src=\"https://pre00.deviantart.net/fcbf/th/pre/f/2012/032/2/6/fierce_deity__s_mask_by_blueamnesiac-d4ob1ax.png\" alt=\"Zelda Diety Mask\"><p>$49.99</p>";
                             }
                             else if ($value === "Keaton Mask") {
                                 echo "<p>Keaton Mask</p>
-                                    <img id=\"pic\" src=\"https://orig00.deviantart.net/2de5/f/2012/020/8/6/keaton_mask_by_blueamnesiac-d4n04xk.png\" alt=\"Zelda Keaton Mask\">";
+                                    <img id=\"pic\" src=\"https://orig00.deviantart.net/2de5/f/2012/020/8/6/keaton_mask_by_blueamnesiac-d4n04xk.png\" alt=\"Zelda Keaton Mask\"><p>$49.99</p>";
                             }
                             else if ($value === "Kafei Mask") {
                                 echo "<p>Kafei Mask</p>
-                                <img id=\"pic\" src=\"https://pre00.deviantart.net/80e5/th/pre/f/2012/025/d/7/kafei__s_mask_by_blueamnesiac-d4nlgmh.png\" alt=\"Zelda Kafei Mask\">";
+                                <img id=\"pic\" src=\"https://pre00.deviantart.net/80e5/th/pre/f/2012/025/d/7/kafei__s_mask_by_blueamnesiac-d4nlgmh.png\" alt=\"Zelda Kafei Mask\"><p>$49.99</p>";
                             }
                             else if ($value === "Gorgon Mask") {
                                 echo "<p>Goron Mask</p>
-                            <img id=\"pic\" src=\"https://pre00.deviantart.net/4b94/th/pre/f/2012/031/3/5/goron_mask_by_blueamnesiac-d4o7875.png\" alt=\"Zelda Goron Mask\">";
+                            <img id=\"pic\" src=\"https://pre00.deviantart.net/4b94/th/pre/f/2012/031/3/5/goron_mask_by_blueamnesiac-d4o7875.png\" alt=\"Zelda Goron Mask\"><p>$49.99</p>";
                             }
                             else if ($value === "Majora's Mask") {
                                 echo "<p>Majora's Mask</p>
-                            <img id=\"pic\" src=\"https://pre00.deviantart.net/b9bb/th/pre/f/2012/036/0/1/majora__s_mask_by_blueamnesiac-d4osuud.png\" alt=\"Zelda Majora's Mask\">";
+                            <img id=\"pic\" src=\"https://pre00.deviantart.net/b9bb/th/pre/f/2012/036/0/1/majora__s_mask_by_blueamnesiac-d4osuud.png\" alt=\"Zelda Majora's Mask\"><p>$49.99</p>";
                             }
                             echo "</td></tr>";
                         }
                     }
                 ?>
         </div>
-        
-        <?php require "bottom.php"?>
     </body>
+    <?php require "bottom.php"?>
 </html>
