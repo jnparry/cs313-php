ae0d973b4dd3c81c0d86f967be8be0c8bc50b9f2
Jordan <par16042@byui.edu>
2018-05-10 20:59:07 -0600

    Shopping

--- web/shop/phpSession.php
+++ web/shop/phpSession.php
@@ -4,16 +4,7 @@
 //    }
 
     $_SESSION['new_val'] = $_POST['name'];
-    $bar = isset($_POST["name"]) ? $_POST["name"] : null;
-
-    var_dump($_SESSION);
     echo $_SESSION['new_val'];
-    if (!(isset($bar))) {
-        echo "Not set.";
-    }
-    else {
-        echo $bar;
-    }
 ?>
 
 <!DOCTYPE html>
