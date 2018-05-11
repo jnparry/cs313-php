6550b873a5db1db3b62155a4e2c21cbf46cd44f8
Jordan <par16042@byui.edu>
2018-05-10 23:36:54 -0600

    View cart

--- web/shop/home.php
+++ web/shop/home.php
@@ -8,7 +8,6 @@
         <meta charset="utf-8">
         <title>Shop | Home</title>
         <link rel="stylesheet" href="index.css">
-        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
     </head>
     
     <body>
