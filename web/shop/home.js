051cfacebd3f98ea7de09204efa44c7abd8440f3
Jordan <par16042@byui.edu>
2018-05-10 19:36:44 -0600

    Shopping

--- web/shop/home.js
+++ web/shop/home.js
@@ -1,15 +1,38 @@
 // Get the modal
 var modal = document.getElementById('add');
 
-// Get the button that opens the modal
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
+// When the user clicks on the more details, open the modal 
 document.getElementById("dekMask").onclick = function() {
     modal.style.display = "block";
     document.getElementById("description").innerHTML = "[Insert description for deku mask]"
+}
+
+document.getElementById("deiMask").onclick = function() {
+    modal.style.display = "block";
+    document.getElementById("description").innerHTML = "[Insert description for deity mask]"
+}
+
+document.getElementById("gorMask").onclick = function() {
+    modal.style.display = "block";
+    document.getElementById("description").innerHTML = "[Insert description for gorgon mask]"
+}
+
+document.getElementById("majMask").onclick = function() {
+    modal.style.display = "block";
+    document.getElementById("description").innerHTML = "[Insert description for majora mask]"
+}
+
+document.getElementById("keaMask").onclick = function() {
+    modal.style.display = "block";
+    document.getElementById("description").innerHTML = "[Insert description for keaton mask]"
+}
+
+document.getElementById("kafMask").onclick = function() {
+    modal.style.display = "block";
+    document.getElementById("description").innerHTML = "[Insert description for kafei mask]"
 }
 
 // When the user clicks on <span> (x), close the modal
