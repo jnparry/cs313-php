336bd94946cb0e372376d4c778e3826e71ad0f8c
Jordan <par16042@byui.edu>
2018-05-11 11:01:40 -0600

    confirm

--- web/shop/checkout.php
+++ web/shop/checkout.php
@@ -1,11 +1,12 @@
 <?php
     session_start();
 
-//    if (!isset($_SESSION['cart']) || count($_SESSION['cart']) == 0) {
+    if (!isset($_SESSION['cart']) || count($_SESSION['cart']) == 0) {
 //        alert("Nothing in cart. Please select an item before checking out.");
 //        header("Location: https://stormy-cove-35722.herokuapp.com/shop/home.php"); /* Redirect browser */
 //        exit();
-//    }
+        echo "wow boy";
+    }
 ?>
 
 <!DOCTYPE html>
