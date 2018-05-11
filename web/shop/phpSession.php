918ce782dcbec80c3d012849cef435883dbdb935
Jordan <par16042@byui.edu>
2018-05-10 23:06:17 -0600

    Shopping

--- web/shop/phpSession.php
+++ web/shop/phpSession.php
@@ -7,13 +7,13 @@
 
     $item = $_POST['yourItem'];
  
-//    $_SESSION['cart'][] = $item;
-//    echo count($_SESSION['cart']);
-//    foreach ($_SESSION['cart'] as $value) {
-//        echo $value;
-//        echo "whatt";
-//        echo $value;
-//    }
+    $_SESSION['cart'][] = $item;
+    echo count($_SESSION['cart']);
+    foreach ($_SESSION['cart'] as $value) {
+        echo $value;
+        echo "whatt";
+        echo $value;
+    }
 ?>
 
 <!DOCTYPE html>
