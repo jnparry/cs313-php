1ccba8bfc31af45b766ecf6db6712d88259e132b
Jordan <par16042@byui.edu>
2018-05-24 17:57:33 -0600

    databases

--- web/homepage/assign.php
+++ web/homepage/assign.php
@@ -11,7 +11,7 @@
         
         <h2>Assignments</h2>
         <span><a href="http://stormy-cove-35722.herokuapp.com/shop/home.php">Shopping Cart</a></span>
-        <span><a href="">Coming soon...</a></span>
+        <span><a href="http://stormy-cove-35722.herokuapp.com/clean/home.php">Database Assignment</a></span>
         <span><a href="">Coming soon...</a></span>
         <span><a href="">Coming soon...</a></span>
         <span><a href="">Coming soon...</a></span>
