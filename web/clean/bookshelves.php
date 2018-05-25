b41bc22dee540105b5f332ed5517208598d8b2f9
Jordan <par16042@byui.edu>
2018-05-25 05:05:09 -0600

    database

--- web/clean/bookshelves.php
+++ web/clean/bookshelves.php
@@ -1,3 +1,28 @@
+<?php
+
+session_start();
+$roomId = $_SESSION['room'];
+
+try {
+    $dbUrl = getenv('HEROKU_POSTGRESQL_CRIMSON_URL');
+    $dbopts = parse_url($dbUrl);
+    
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+    
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+}
+catch (PDOException $ex){
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,16 +33,23 @@
     </head>
     
     <body>
-        <?php require ""?>
         <h1>Selected Project - Selected Room</h1>
         <section>
             <button onClick="viewRooms()">Back to Rooms</button>
-            <button>Add Bookshelf</button>
         </section>
+        
         <section>
+            <?php
+                foreach ($db->query("SELECT * FROM bookshelves WHERE roomsid = '$roomId'") as $row) {
+                    echo "<p>We have a bookcase. Its coordinates are (" . $row['x'] . ", " . $row['y'] . ")."; 
+                }
+            ?>
+            
             Bookshelf image goes here
             <section>
-                Number of Shelves:
+                <p>Number of shelves</p>
+                <input type="number" min="0" max="10">
+                <button>Add Bookshelf</button>
             </section>
         </section>
     </body>
--- web/clean/phpSession.php
+++ web/clean/phpSession.php
@@ -3,21 +3,18 @@
 
     if (isset($_POST['projectId'])) {
         $_SESSION['project'] = $_POST['projectId'];
-        
-        echo "ProjectId set";
+
         /* Redirect browser */
         header("Location: https://stormy-cove-35722.herokuapp.com/clean/rooms.php");
         exit();
     } elseif (isset($_POST['roomId'])) {
         $_SESSION['room'] = $_POST['roomId'];
         
-        echo "RoomId set";
         /* Redirect browser */
         header("Location: https://stormy-cove-35722.herokuapp.com/clean/bookshelves.php");
         exit();
     } elseif (isset($_POST['bookshelfId'])) {
         $_SESSION['bookshelf'] = $_POST['bookshelfId'];
         
-        echo "BookshelfId set";
     }
 ?>
