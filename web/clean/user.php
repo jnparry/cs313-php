f8ae912387f4aca8beb4ce9af12e51dc87475ed1
Jordan <par16042@byui.edu>
2018-05-25 06:44:57 -0600

    database begin styling

--- web/clean/user.php
+++ web/clean/user.php
@@ -1,6 +1,6 @@
 <nav>
     <h1 id="">McKay Library Cleaning</h1>
-    <section style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</section>
+    <section id="menu" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</section>
 </nav>
 
 <?php require "userOptions.php"?>
