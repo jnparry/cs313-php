f836f488473c22757bd4f7d3be637f82239690b9
Jordan <par16042@byui.edu>
2018-05-24 22:40:08 -0600

    database

--- web/clean/rooms.php
+++ web/clean/rooms.php
@@ -1,26 +1,4 @@
-<?php
 
-session_start();
-
-try {
-    $dbUrl = getenv('HEROKU_POSTGRESQL_CRIMSON_URL');
-    $dbopts = parse_url($dbUrl);
-    
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"],'/');
-    
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
-}
-catch (PDOException $ex){
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-
-?>
 
 <!DOCTYPE html>
 <html>
