97f21e9fbfe2e75a631bd10e7fb43e4ef22f3355
Jordan <par16042@byui.edu>
2018-05-24 22:48:56 -0600

    database

--- web/clean/home.js
+++ web/clean/home.js
@@ -1,7 +1,5 @@
 /* nothing yet. */
 function loadRooms(projectId) {
     location.href = "/clean/rooms.php";
-    <?php
-        var trial = "This is my php inside of js...";
-    ?>
+    var trial = "<?php This is my php inside of js... ?>";
 }
