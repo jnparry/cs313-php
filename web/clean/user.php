488a84e6739c22e50b8d0a914f792fdcee108782
Jordan <par16042@byui.edu>
2018-05-25 08:46:50 -0600

    database begin styling

--- web/clean/user.php
+++ web/clean/user.php
@@ -1,5 +1,5 @@
 <nav>
-    <a href="https://byui.edu"><img class="logo" src="https://inet.byui.edu/sites/Identity/Official%20Logo/White%20Two%20Line%20Monogram/PNG/TwoLineMonogram_white_Large.png" alt="BYU-Idaho logo"></a>
+    <a href="https://byui.edu"><img class="logo" src="http://abish.byui.edu/library/images/byui-logo.png" alt="BYU-Idaho logo"></a>
     <h1 id="heading">McKay Library Cleaning</h1>
     <section id="menu" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</section>
 </nav>
