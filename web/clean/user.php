c3635eec5d4c53978306b4e860a4cf96470f84f3
Jordan <par16042@byui.edu>
2018-05-30 13:02:28 -0600

    Mobile view

--- web/clean/user.php
+++ web/clean/user.php
@@ -1,7 +1,7 @@
 <nav>
     <a href="/clean/home.php"><img class="logo" src="http://abish.byui.edu/library/images/byui-logo.png" alt="BYU-Idaho logo"></a>
     <h1 id="heading">McKay Library Cleaning</h1>
-    <section id="menu" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</section>
+    <section id="menu" style="font-size:30px;cursor:pointer" onclick="openNav()"><p>&#9776;</p></section>
 </nav>
 
 <?php require "userOptions.php"?>
