4c033b98b989d03069c99364c76dc15ea6d7e667
Jordan <par16042@byui.edu>
2018-05-30 17:51:41 -0600

    Mobile view

--- web/clean/view.php
+++ web/clean/view.php
@@ -51,9 +51,6 @@ catch (PDOException $ex){
         <section>
             <form action="phpSession.php" method="post" name="rooms">
                 <?php
-                SELECT *
-    FROM weather w, cities c
-    WHERE w.city = c.name;
                     foreach ($db->query("SELECT * FROM bookshelves b, shelves s WHERE roomsid = '$roomId' AND b.id = s.bookshelvesid") as $row) {
                         echo "<button type='submit' value='$roomId' name='roomId'>Add Bookshelf</button>";
                         echo "<p>We have a bookcase. Its coordinates are (" . $row['x'] . ", " . $row['y'] . ").";
