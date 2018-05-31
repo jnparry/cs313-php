e659489e9be662632719c3c85c1bdd6f217dd893
Jordan <par16042@byui.edu>
2018-05-31 10:41:33 -0600

    Mobile view

--- web/clean/user.php
+++ web/clean/user.php
@@ -1,4 +1,4 @@
-<nav>
+<nav id="topHeader">
     <a href="/clean/home.php"><img class="logo" src="http://abish.byui.edu/library/images/byui-logo.png" alt="BYU-Idaho logo"></a>
     <h1 id="heading">McKay Library Cleaning</h1>
     <section id="menu" style="font-size:30px;cursor:pointer" onclick="openNav()"><p id="menu_button">&#9776;</p></section>
