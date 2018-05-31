156029d4bdcee3dbfd666472b992d2b5f9f697bf
Jordan <par16042@byui.edu>
2018-05-31 16:07:53 -0600

    Canvas

--- web/clean/view.php
+++ web/clean/view.php
@@ -51,9 +51,9 @@ catch (PDOException $ex){
 
             <section>
                 <form action="phpSession.php" method="post" name="rooms">
-                    <button type='submit' value='$roomId' name='roomId'>Add Bookshelf</button>
-                    <canvas id="myCanvas" onload="setUpCanvas()"></canvas>
-                        <?php
+                <?php
+                    echo "<button type='submit' value='$roomId' name='roomId'>Add Bookshelf</button>";
+                    echo "<canvas id='myCanvas' onload='setUpCanvas()'></canvas>";
                             foreach ($db->query("SELECT * FROM bookshelves b, shelves s WHERE roomsid = '$roomId' AND b.id = s.bookshelvesid") as $row) {
                                 echo "<p>We have a bookcase. Its coordinates are (" . $row['x'] . ", " . $row['y'] . ").";
                                 echo "<p>This shelf ";
