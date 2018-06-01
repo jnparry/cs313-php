bfb83b572eb135f91b522e97f954fae776e92ce1
Jordan <par16042@byui.edu>
2018-05-31 23:18:30 -0600

    Bookcases?

--- web/clean/home.js
+++ web/clean/home.js
@@ -30,9 +30,38 @@ function soon() {
 
 function setUpCanvas() {
     var canvas = document.getElementById("myCanvas");
-//    canvas.width = window.innerWidth;
-    var ctx = canvas.getContext('2d');
-    ctx.moveTo(0,0);
-    ctx.lineTo(200,100);
-    ctx.stroke();
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    
+    var stage = new Konva.Stage({
+        container: 'container',
+        width: width,
+        height: height
+    });
+    
+    var layer = new Konva.Layer();
+    var rectX = stage.getWidth() / 2 - 50;
+    var rectY = stage.getHeight() / 2 - 25;
+    
+    var box = new Konva.Rect({
+        x: rectX,
+        y: rectY,
+        width: 100,
+        height: 50,
+        fill: '#00D2FF',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable: true
+    });
+    
+    // add cursor styling
+    box.on('mouseover', function() {
+        document.body.style.cursor = 'pointer';
+    });
+    box.on('mouseout', function() {
+        document.body.style.cursor = 'default';
+    });
+    
+    layer.add(box);
+    stage.add(layer);
 }
