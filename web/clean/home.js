7afc6bc1a206fb21e192f8ec1a7bcee3ed33fe88
Jordan <par16042@byui.edu>
2018-06-02 13:51:18 -0600

    Insert data

--- web/clean/home.js
+++ web/clean/home.js
@@ -46,16 +46,16 @@ function mouse(item, event) { // (1) start the process
     item.style.zIndex = 1000;
     // move it out of any current parents directly into body
     // to make it positioned relative to the body
-//    document.body.append(item);
+    document.body.append(item);
     // ...and put that absolutely positioned ball under the cursor
 
     moveAt(event.pageX, event.pageY);
 
     // centers the ball at (pageX, pageY) coordinates
-    function moveAt(pageX, pageY) {
-    item.style.left = pageX - item.offsetWidth / 2 + 'px';
-    item.style.top = pageY - item.offsetHeight / 2 + 'px';
-    }
+//    function moveAt(pageX, pageY) {
+//    item.style.left = pageX - item.offsetWidth / 2 + 'px';
+//    item.style.top = pageY - item.offsetHeight / 2 + 'px';
+//    }
 
     function onMouseMove(event) {
     moveAt(event.pageX, event.pageY);
