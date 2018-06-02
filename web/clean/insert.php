cf08f16f721aee6ad6c7c7ceb45294f91fa02a4b
Jordan <par16042@byui.edu>
2018-06-02 11:04:11 -0600

    Insert data

--- web/clean/insert.php
+++ web/clean/insert.php
@@ -3,11 +3,12 @@
     if (isset($_POST['txtTitle'])) {    
         $title = $_POST['txtTitle'];
         echo "In if";
-        echo $title;
+        echo "$title";
     }
 
     // we could (and should!) put additional checks here to verify that all this data is actually provided
-    echo $title;
+    echo "out of if ";
+    echo "$title";
 
     require("dbConnect.php");
     $db = get_db();
