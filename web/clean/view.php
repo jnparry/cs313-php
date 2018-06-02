bc038120a0b75cee196b0f7a64868aa4da7de63d
Jordan <par16042@byui.edu>
2018-06-01 22:17:51 -0600

    Drag and drop

--- web/clean/view.php
+++ web/clean/view.php
@@ -59,6 +59,7 @@ catch (PDOException $ex){
                     <?php
                         foreach ($db->query("SELECT * FROM bookshelves b, shelves s WHERE roomsid = '$roomId' AND b.id = s.bookshelvesid") as $row) {
                             echo "<p>We have a bookcase. Its coordinates are (" . $row['x'] . ", " . $row['y'] . ").";
+                            echo "<button type='button' style='position: relative; right: " . $row['x'] . "; top: " . $row['y'] . ";'>This is my bookcase</button>";
                             echo "<p>This shelf ";
                             if ($row['shelvesclean'] && $row['shelvesdate']) {
                                 echo "was cleaned " . $row['shelvesdate'] . "</p>";
