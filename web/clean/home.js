9b5a0aa688e3570a040ee7fef9b917ddc1f35240
Jordan <par16042@byui.edu>
2018-06-02 14:12:25 -0600

    Insert data

--- web/clean/home.js
+++ web/clean/home.js
@@ -47,28 +47,29 @@ function mouse(item, event) { // (1) start the process
     item.style.zIndex = 1000;
     // move it out of any current parents directly into body
     // to make it positioned relative to the body
-    document.body.append(item);
+    document.getElementById("area").append(item);
     // ...and put that absolutely positioned ball under the cursor
 
     moveAt(event.pageX, event.pageY);
 
     // centers the ball at (pageX, pageY) coordinates
     function moveAt(pageX, pageY) {
-    item.style.left = pageX - item.offsetWidth / 2 + 'px';
-    item.style.top = pageY - item.offsetHeight / 2 + 'px';
+        item.style.left = pageX - item.offsetWidth / 2 + 'px';
+        item.style.top = pageY - item.offsetHeight / 2 + 'px';
     }
 
     function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
+        moveAt(event.pageX, event.pageY);
     }
 
     // (3) move the ball on mousemove
-    document.addEventListener('mousemove', onMouseMove);
+    document.getElementById("area").addEventListener('mousemove', onMouseMove);
 
     // (4) drop the ball, remove unneeded handlers
     item.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    item.onmouseup = null;
+        alert("fudgeee");
+        document.getElementById("area").removeEventListener('mousemove', onMouseMove);
+        item.onmouseup = null;
     };
 
     item.ondragstart = function() {
