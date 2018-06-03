d58cc342fb8749326e317dbab524c935a5079af2
Jordan <par16042@byui.edu>
2018-06-03 12:04:01 -0600

    Updating

--- web/clean/projects.php
+++ web/clean/projects.php
@@ -36,7 +36,8 @@
                             echo "<p class='middle'>Cleaning incomplete.</p>";
                         }
                         
-                        echo "<button class='last' type='submit' value='" . $row['id'] . "' name='projectId'>View</button>";
+                        echo "<button class='last' type='submit' value='" . 
+                            $row['id'] . "' name='projectId'>View</button>";
                         echo "<button class='last' type='button' onclick=\"showForm('hiddenForm', 'txtTitle', " . $row['name'] . ")\">Edit</button>";
                         echo "</li>";
                     }
