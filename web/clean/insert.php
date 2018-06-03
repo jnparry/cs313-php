6f4cad3d74681ea76479856073386365f5aed788
Jordan <par16042@byui.edu>
2018-06-03 13:05:44 -0600

    Updating

--- web/clean/insert.php
+++ web/clean/insert.php
@@ -3,53 +3,57 @@
 session_start();
 $projectId = $_SESSION['project'];
 
-if ($_POST['rename']) {
+if ($_POST['rename'])
     $submitType = $_POST['rename'];
-    if ($submitType == 'rename')
-        echo "equal equal s q rename";
-}
-if ($_POST['add']) {
+if ($_POST['add'])
     $submitType = $_POST['add'];
-    if ($submitType == "add")
-        echo "equal euqal db q add";
-}
 
-//require("dbConnect.php");
-//$db = get_db();
-//
-//if (isset($_POST['pTitle'])) { 
-//    if ($submitType )
-//    try {
-//        $title = $_POST['pTitle'];
-//        $query = "INSERT INTO projects(name, iscomplete, date) VALUES(:name, FALSE, NULL)";
-//
-//        $statement = $db->prepare($query);
-//        $statement->bindValue(':name', $title);
-//        $statement->execute();
-//    } catch (Exception $ex) {
-//        echo "Error with DB. Details: $ex";
-//        die();
-//    }
-//    
-//    header("Location: /clean/projects.php");
-//    die();
-//}
-//else if (isset($_POST['rTitle'])) {
-//    try {
-//        $title = $_POST['rTitle'];
-//        $query = "INSERT INTO rooms(name, projectsid, isclean, date) VALUES(:name, :pId, FALSE, NULL)";
-//
-//        $statement = $db->prepare($query);
-//        $statement->bindValue(':name', $title);
-//        $statement->bindValue(':pId', $projectId);
-//        $statement->execute();
-//    } catch (Exception $ex) {
-//        echo "Error with DB. Details: $ex";
-//        die();
-//    }
-//    
-//    header("Location: /clean/rooms.php");
-//    die();
-//}
+require("dbConnect.php");
+$db = get_db();
+
+if (isset($_POST['pTitle'])) {
+    $title = $_POST['pTitle'];
+    
+    try {
+        if ($submitType == "add") {
+            $query = "INSERT INTO projects(name, iscomplete, date) VALUES(:name, FALSE, NULL)";
+
+            $statement = $db->prepare($query);
+            $statement->bindValue(':name', $title);
+            $statement->execute();
+        }
+        else if ($submitType == "rename") {
+            $query = "UPDATE projects SET name = ':name' WHERE id = ':pId'";
+
+            $statement = $db->prepare($query);
+            $statement->bindValue(':name', $title);
+            $statement->bindValue(':pId', $projectId);
+            $statement->execute();
+        }
+    } catch (Exception $ex) {
+            echo "Error with DB. Details: $ex";
+            die();
+    } 
+    
+    header("Location: /clean/projects.php");
+    die();
+}
+else if (isset($_POST['rTitle'])) {
+    try {
+        $title = $_POST['rTitle'];
+        $query = "INSERT INTO rooms(name, projectsid, isclean, date) VALUES(:name, :pId, FALSE, NULL)";
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':name', $title);
+        $statement->bindValue(':pId', $projectId);
+        $statement->execute();
+    } catch (Exception $ex) {
+        echo "Error with DB. Details: $ex";
+        die();
+    }
+    
+    header("Location: /clean/rooms.php");
+    die();
+}
 
 ?>
