2b3f8e2c53cf1a282012a649e972a775fcd3a238
Jordan <par16042@byui.edu>
2018-06-02 22:30:49 -0600

    Updating

--- web/clean/home.js
+++ web/clean/home.js
@@ -78,11 +78,11 @@ function mouse(item, event) { // (1) start the process
         }
         if (xc - (item.offsetWidth / 2) <= left) {
             xc = left + (item.offsetWidth);
-//            document.removeEventListener('mousemove', onMouseMove);
-//            item.onmouseup = null;  
+            document.removeEventListener('mousemove', onMouseMove);
+            item.onmouseup = null;  
         }
-        if (yc + (item.offsetHeight / 2) >= area.getBoundingClientRect().bottom) {
-            yc = area.getBoundingClientRect().bottom - (item.offsetHeight);
+        if (yc + (item.offsetHeight / 2) >= bottom) {
+            yc = bottom - (item.offsetHeight);
 //            document.removeEventListener('mousemove', onMouseMove);
 //            item.onmouseup = null;
         }
