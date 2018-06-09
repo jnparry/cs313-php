4d8231218f988c1ed5e1be2d759ba80c2fea3b60
Jordan <par16042@byui.edu>
2018-06-09 16:14:18 -0600

    bookshelves.php

--- web/clean/home.js
+++ web/clean/home.js
@@ -68,8 +68,8 @@ function showForm(divName, editName, val, pId = null) {
 function saveChanges() {
     var x = document.getElementsByClassName("cases");
     for (var i = 0; i < x.length; i++) {
-        var pos = x[1].style;
-        alert("Top: " . pos.top);
+//        var pos = x[1].style;
+        alert("Top: " . x[i].style.top);
     }
 }
      
