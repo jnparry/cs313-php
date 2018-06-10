47e998be3380d971ec0c5ee1562cd848b6efc21a
Jordan <par16042@byui.edu>
2018-06-09 20:43:47 -0600

    bookshelves.php

--- web/clean/home.js
+++ web/clean/home.js
@@ -101,8 +101,8 @@ function mouse(item, event) {
 
     // centers the ball at (pageX, pageY) coordinates
     function moveAt(pageX, pageY) {
-        item.style.left = pageX + 'px';
-        item.style.top = pageY + 'px';
+        item.style.left = pageX - offset.left - item.offsetWidth / 2 + 'px';
+        item.style.top = pageY - item.offset.top - item.offsetHeight / 2 + 'px';
     }
 
     function onMouseMove(event) {
