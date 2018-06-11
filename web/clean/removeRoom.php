82dba41be283f8c4b022e134a66390da3dfacf70
Jordan <par16042@byui.edu>
2018-06-11 11:24:29 -0600

    Catching errors

--- web/clean/removeRoom.php
+++ web/clean/removeRoom.php
@@ -13,6 +13,28 @@ $roomId = $_SESSION['deleteId'];
 require("dbConnect.php");
 $db = get_db();
 
+// CHECK IF THERE ARE ANY BOOKSHELVES REFERENCING THIS ROOM
+try {
+    $query = "SELECT * FROM bookshelves";
+
+    $statement = $db->prepare($query);
+    $statement->execute();
+    
+    $_SESSION["error"] = false;
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        if ($row["roomsid"] == $roomId) {
+            $_SESSION["error"] = true;
+            $_SESSION["msg"] = "Cannot delete because the room contains bookcases. Delete the bookcases before deleting the room.";
+            header("Location: /clean/rooms.php");
+            die();
+        }
+    }
+} catch (Exception $ex) {
+    echo "Error with DB. Details: $ex";
+    die();
+} 
+
+// IF THERE ARE NO BOOKCASES REFERENCING THIS ROOM, DELETE IT
 try {
     $query = "DELETE FROM rooms WHERE id = :rId";
 
