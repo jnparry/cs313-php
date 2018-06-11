c78f7cd743c0f9bff9e7a97cd6c9e9212fb81ca1
Jordan <par16042@byui.edu>
2018-06-11 11:21:28 -0600

    Delete btn

--- web/clean/remove.php
+++ web/clean/remove.php
@@ -11,7 +11,30 @@ $projectId = $_SESSION['deleteId'];
 
 require("dbConnect.php");
 $db = get_db();
+ 
+// CHECK IF THERE ARE ANY ROOMS REFERENCING THIS PROJECT
+try {
+    $query = "SELECT * FROM rooms";
 
+    $statement = $db->prepare($query);
+    $statement->execute();
+    
+    $_SESSION["error"] = false;
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        if ($row["projectsid"] == $projectId) {
+            $_SESSION["error"] = true;
+            $_SESSION["msg"] = "Cannot delete because the project contains rooms. Delete the rooms before deleting the project.";
+            header("Location: /clean/projects.php");
+            die();
+        }
+    }
+} catch (Exception $ex) {
+    echo "Error with DB. Details: $ex";
+    die();
+} 
+
+
+// IF THERE ARE NO TABLES REFERENCING THIS COLUMN, DELETE IT
 try {
     $query = "DELETE FROM projects WHERE id = :pId";
 
