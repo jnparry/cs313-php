6e021a46244a6db4d8ebce809ab4d9df2c3c8c6b
Jordan <par16042@byui.edu>
2018-06-19 13:46:51 -0600

    bookshelves.php

--- web/clean/home.js
+++ web/clean/home.js
@@ -110,7 +110,7 @@ function savePos(item, event) {
     publicX = event.pageX;
     publicY = event.pageY;
     alert(publicX + ", " + publicY);
-    alert(item.offsetWidth);
+    alert(item.offsetWidth / 2);
 }
 
 // for desktop w/ mouse click events
