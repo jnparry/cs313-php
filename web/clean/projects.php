a1cfe8092f2400c9a6c09b004c827aff26d5ed4a
Jordan <par16042@byui.edu>
2018-06-19 10:10:30 -0600

    Position

--- web/clean/projects.php
+++ web/clean/projects.php
@@ -16,53 +16,55 @@
     </head>
     
     <body>
-        <?php require "user.php"?>
-        <h2>Current Cleaning Projects</h2>
-        <?php
-            if ($_SESSION["error"]) {
-                echo "<p style='color: red; text-align: center;'>*Error: " . $_SESSION["msg"] . "</p>";
-                $_SESSION["error"] = false;
-            }
-        ?>
-        
-        <form action="phpSession.php" method="post" name="projects">
-            <ul>
-                <?php
-                    $statement = $db->prepare("SELECT * FROM projects ORDER BY id");
-                    $statement->execute();
-     
-                    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                        echo "<li>";
-                        echo "<p class='first'><strong>" . $row['name'] . "</strong></p>";
-                        
-                        if ($row['iscomplete'] && $row['date']) {
-                            echo "<p class='middle'>Cleaning completed " . $row['date'] . "</p>";
+        <section class="content">
+            <?php require "user.php"?>
+            <h2>Current Cleaning Projects</h2>
+            <?php
+                if ($_SESSION["error"]) {
+                    echo "<p style='color: red; text-align: center;'>*Error: " . $_SESSION["msg"] . "</p>";
+                    $_SESSION["error"] = false;
+                }
+            ?>
+
+            <form action="phpSession.php" method="post" name="projects">
+                <ul>
+                    <?php
+                        $statement = $db->prepare("SELECT * FROM projects ORDER BY id");
+                        $statement->execute();
+
+                        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                            echo "<li>";
+                            echo "<p class='first'><strong>" . $row['name'] . "</strong></p>";
+
+                            if ($row['iscomplete'] && $row['date']) {
+                                echo "<p class='middle'>Cleaning completed " . $row['date'] . "</p>";
+                            }
+                            else {
+                                echo "<p class='middle'>Cleaning incomplete.</p>";
+                            }
+
+                            echo "<button class='last' type='submit' value='" . $row['id'] . "' name='projectId'>View</button>";
+                            echo "<button class='last' type='button' onclick=\"showForm('hiddenForm', 'txtTitle', '" . $row['name'] . "', '" . $row['id'] . "')\">Edit</button>";
+                            echo "</li>";
                         }
-                        else {
-                            echo "<p class='middle'>Cleaning incomplete.</p>";
-                        }
-                        
-                        echo "<button class='last' type='submit' value='" . $row['id'] . "' name='projectId'>View</button>";
-                        echo "<button class='last' type='button' onclick=\"showForm('hiddenForm', 'txtTitle', '" . $row['name'] . "', '" . $row['id'] . "')\">Edit</button>";
-                        echo "</li>";
-                    }
-                ?>
-            </ul>
-        </form>
-        
-        <section class="bottomNav">
-                <button onclick="viewHome()">&#10094; Back to Home</button>
-                <button type="button" onclick="showForm('hiddenForm', 'txtTitle', 'add')">Add Project</button>
-            
-            <form action="insert.php" method="post" name="add" id="hiddenForm" style="display: none;">
-                <br/><br/><br/>
-                <label id="anchor" for="txtTitle">Project Name: </label>	
-                <input type="text" id="txtTitle" name="pTitle">
-                <br/><br/>
-                <button type="submit" id="submit">Submit</button>
-                <button type="submit" id="remove" name="removeP" style="display: none;">Delete</button>
+                    ?>
+                </ul>
             </form>
-            
+
+            <section class="bottomNav">
+                    <button onclick="viewHome()">&#10094; Back to Home</button>
+                    <button type="button" onclick="showForm('hiddenForm', 'txtTitle', 'add')">Add Project</button>
+
+                <form action="insert.php" method="post" name="add" id="hiddenForm" style="display: none;">
+                    <br/><br/><br/>
+                    <label id="anchor" for="txtTitle">Project Name: </label>	
+                    <input type="text" id="txtTitle" name="pTitle">
+                    <br/><br/>
+                    <button type="submit" id="submit">Submit</button>
+                    <button type="submit" id="remove" name="removeP" style="display: none;">Delete</button>
+                </form>
+
+            </section>
         </section>
     </body>
 </html>
