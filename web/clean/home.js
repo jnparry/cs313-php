0790c9e97243687194f8a13cb173b2bcd9d442b6
Jordan <par16042@byui.edu>
2018-07-10 10:13:06 -0600

    Touches

--- web/clean/home.js
+++ web/clean/home.js
@@ -210,7 +210,8 @@ function touch(item, event, id) {
     item.style.zIndex = 1000;
 //    item.style.position = fixed;
 
-    moveAt(event.touches[0].clientX, event.touches[0].clientY);
+    console.log(event.touches[0].clientX + ", " + event.touches[0].clientY);
+//    moveAt(event.touches[0].clientX, event.touches[0].clientY);
 
     // centers the ball at (pageX, pageY) coordinates
     function moveAt(pageX, pageY) {
