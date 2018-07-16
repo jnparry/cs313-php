c6694cfc015b429da7b53079073d9a5a8cb79f28
Jordan <par16042@byui.edu>
2018-07-16 16:37:10 -0600

    signin

--- web/clean/home.js
+++ web/clean/home.js
@@ -330,7 +330,7 @@ function touch(item, event, id) {
             
             if ( !(rect1.right < rect2.left || rect1.left > rect2.right || rect1.bottom < rect2.top || rect1.top > rect2.bottom)) {
                 overlap = true;
-                console.log("Overlapping " . (rect1.right));
+                console.log("Overlapping");
             } else { // if no overlap; one or more is true
                 overlap = false;
             }
