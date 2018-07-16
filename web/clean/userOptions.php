9f000722bd43d6cc84c0f5f195b973f4eec882e5
Jordan <par16042@byui.edu>
2018-07-16 13:26:19 -0600

    signin

--- web/clean/userOptions.php
+++ web/clean/userOptions.php
@@ -6,7 +6,7 @@
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>    
     <?
     if (isset($_SESSION['user'])) {
-        echo "Welcome " . $_SESSION['user'];
+        echo "<a>Welcome " . $_SESSION['user'] . "</a>";
         echo "<a href='/clean/home.php'>Home</a>";
         echo "<a href='/clean/projects.php'>View Projects</a>";
         echo "<a href='#'>Settings</a>";
