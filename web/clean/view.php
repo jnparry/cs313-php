aee82755f31aaad863d745f8b7b061eada585f23
Jordan <par16042@byui.edu>
2018-07-19 10:37:37 -0600

    num items

--- web/clean/view.php
+++ web/clean/view.php
@@ -136,21 +136,21 @@
                             if (!$notClean)
                                 echo "Clean";
                             echo"</button>";    
-                            
-                            echo '<div class="modal-content">
-                              <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">This is my modal.</h4>
-                              </div>
-                              <div class="modal-body">
-                                <p>Its so easy when we use bootstrap!</p>
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                              </div>
-                            </div>';
                         }
                     ?>
+                    
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">This is my modal.</h4>
+                      </div>
+                      <div class="modal-body">
+                        <p>Its so easy when we use bootstrap!</p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>;
                 </section>
 
                 <section class="bottomNav">
